443e38b633b96dbd71056970eaf951c29ecd2872
Kevin Bowling <kevin.bowling@kev009.com>
2014-10-03 23:29:24 -0700

    Update to 2014.7.0rc3

--- files/patch-salt__grains__core.py
+++ files/patch-salt__grains__core.py
@@ -1,11 +1,12 @@
---- salt/grains/core.py.orig	2014-09-16 16:31:27.506893540 -0700
-+++ salt/grains/core.py	2014-09-16 16:36:12.310876906 -0700
-@@ -1230,7 +1230,7 @@
+--- salt/grains/core.py.orig	2014-10-03 23:23:07.591112328 -0700
++++ salt/grains/core.py	2014-10-03 23:23:34.663110325 -0700
+@@ -1232,7 +1232,8 @@
      '''
      # Provides:
      #   path
 -    return {'path': os.environ['PATH'].strip()}
-+    return {'path': os.environ['PATH'].strip() + ':/usr/local/bin:/usr/local/sbin'}
++    return {'path': os.environ['PATH'].strip() +
++            ':/usr/local/bin:/usr/local/sbin'}
  
  
  def pythonversion():
--- files/patch-setup.py
+++ files/patch-setup.py
@@ -1,14 +1,15 @@
---- ./setup.py.orig	2014-03-06 10:50:27.000000000 +0200
-+++ ./setup.py	2014-03-06 10:52:20.000000000 +0200
-@@ -451,12 +451,12 @@
-                                     'virt/*.jinja'
-                                     ],
+--- setup.py.orig	2014-10-03 13:10:27.000000000 -0700
++++ setup.py	2014-10-03 23:27:43.010093118 -0700
+@@ -666,13 +666,13 @@
+                                     '*.flo'
+                                     ]
                                  },
 -                'data_files': [('share/man/man1',
 +                'data_files': [('man/man1',
                                  ['doc/man/salt-cp.1',
                                   'doc/man/salt-call.1',
                                   'doc/man/salt-minion.1',
+                                  'doc/man/salt-unity.1',
                                   ]),
 -                               ('share/man/man7',
 +                               ('man/man7',
